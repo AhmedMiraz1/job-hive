a4034182f789e032bd97059d39007851e4996b48
mmirazz <mehedimiraz900@gmail.com>
2024-05-26 17:15:07 +0600

    job gallery section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 
 import Banner from "../componenets/Banner";
 import JobCategory from "../componenets/JobCategory";
+import JobGallery from "../componenets/JobGallery";
+
 
 
 const Home = () => {
@@ -11,6 +13,10 @@ const Home = () => {
            <div >
            <Banner/>
            <JobCategory />
+           </div>
+
+           <div>
+            <JobGallery/>
            </div>
         </div>
     );
